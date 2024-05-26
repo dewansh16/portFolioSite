cdc43bdaaa7a5a4410394287e56475b7267c6554
Dewansh Patle <dewanshpatle69@gmail.com>
2024-05-26 17:25:49 +0530

    removed commas

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -59,7 +59,7 @@ export default function Intro() {
         className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}>
-        <span className="font-bold">Hello, I'm Ricardo.</span> I'm a{" "}
+        <span className="font-bold">Hello, I&apos;m Ricardo.</span> I&apos;m a{" "}
         <span className="font-bold">full-stack developer</span> with{" "}
         <span className="font-bold">8 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
